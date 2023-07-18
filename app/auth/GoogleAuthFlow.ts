211da75b5aaaec7686708a4963d063643438a76c
Dominic Nidy <dominicnidy3@gmail.com>
2023-07-18 18:06:19 -0400

    Working on creating custom user collection in firestore. This custom user object will contain the data retreived from the authentication sign in

--- app/auth/GoogleAuthFlow.ts
+++ app/auth/GoogleAuthFlow.ts
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  User,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 export const firebase_options = {
   apiKey: "AIzaSyAPczHoT5cJ1fxv4fk_fQjnRHaL8WXPX-o",
@@ -10,27 +16,47 @@ export const firebase_options = {
   appId: "1:296730327999:web:74c09b878bd58e8a28ff0a",
   measurementId: "G-V87LXV2M29",
 };
+// Initialize Firebase
+const app = initializeApp(firebase_options);
 
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Writes a temp spotify token to database
+async function setUserDocument(user: User) {
+  const userData = user.toJSON();
+
+  await setDoc(doc(db, "users", `${user.uid}`), {
+    displayName: user.displayName,
+    uid: user.uid,
+    email: user.email,
+    emailVerified: user.emailVerified,
+    creationTime: user.metadata.creationTime,
+    lastSignIn: user.metadata.lastSignInTime,
+  });
+}
 // Returns the user object if successful, otherwise returns undefined
 export async function loginGoogle() {
-  const app = initializeApp(firebase_options);
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
   return signInWithPopup(auth, provider)
-    .then((result) => {
+    .then(async (result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential!.accessToken;
       // The signed-in user info.
       const user = result.user;
+
+      // Update user document on each sign in
+      await setUserDocument(user);
+
       return user;
     })
     .catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
+
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
