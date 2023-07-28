2d6070818279f5255bcf87d7612130e218dff610
Dominic Nidy <dominicnidy3@gmail.com>
2023-07-27 22:50:59 -0400

    Using firebase admin sdk to initialize the app (for use in verifying id tokens)

--- app/api/user/spotify/playlists/route.ts
+++ app/api/user/spotify/playlists/route.ts
@@ -1,7 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSpotifyToken } from "@/app/firebase/SpotifyTokens";
-import { auth } from "firebase-admin";
+import { auth, credential, initializeApp } from "firebase-admin";
 import { IdTokenIsValid } from "@/app/firebase/Authorization";
+import { ServiceAccount, applicationDefault, getApp } from "firebase-admin/app";
+import * as admin from "firebase-admin";
+
+admin.initializeApp({
+  credential: credential.cert({
+    type: process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+    project_id: process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+    token_uri: process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url:
+      process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+  } as ServiceAccount),
+  databaseURL: "https://multi-migrate-default-rtdb.firebaseio.com",
+});
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const id_token = req.headers.get("idtoken") as string;
@@ -41,7 +62,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
   }
 
   console.log(token);
-  // If our token exists and is not expired
+  // If our token exists
   if (
     token instanceof Object &&
     "access_token" in token &&
