592dba4154f91788f94af72e1b6971e77e610aff
Dominic Nidy <dominicnidy3@gmail.com>
2023-10-02 11:02:40 -0400

    Deleted the old transfer page

--- app/dashboard/transfertest/page.tsx
+++
@@ -1,9 +0,0 @@
-import TransferPlaylistForm from "@/components/dashboard/transfer-form/TransferPlaylistForm";
-
-export default function TransferPage() {
-  return (
-    <main className="min-h-screen max-h-screen flex  justify-center relative top-8 ">
-      <TransferPlaylistForm />
-    </main>
-  );
-}
